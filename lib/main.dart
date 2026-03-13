18f5e94ac4f3a920ebfde87aba2b93dc3eb22a05
perra <giacomoperra16@gmail.com>
2026-03-13 18:30:12 +0100

    commit n°3

--- lib/main.dart
+++ lib/main.dart
@@ -4,30 +4,40 @@ void main() {
   runApp(
     MaterialApp(
       title: "bella",
-
-      home: Scaffold(
-       // backgroundColor: Colors.amberAccent,
-
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromRGBO(1, 92, 182, 0.69),
-                Color.fromRGBO(55, 76, 97, 0.69),
-              ],
-              begin: AlignmentGeometry.center,
-              end: AlignmentGeometry.centerEnd
-            ),
-          ),
-          child: bodymain()
-        ),
-      ),
-    ),
+      home: MyApp())
+      
   );
 }
 
 Widget bodymain() {
   return Center(
-    child: Text("ss",style: TextStyle(fontSize: 28,color: Colors.white),),
+    child: Text("0",style: TextStyle(fontSize: 28,color: Colors.white),),
   );
+}
+
+class MyApp extends StatelessWidget{
+
+
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+       // backgroundColor: Colors.amberAccent,
+
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: RadialGradient(
+              colors: [
+                Color.fromRGBO(1, 92, 182, 0.69),
+                Color.fromRGBO(55, 76, 97, 0.69),
+              ],
+              center: AlignmentGeometry.center,
+              radius: 0.9
+
+            ),
+          ),
+          child: bodymain()
+        ),
+      );
+  }
+
 }
